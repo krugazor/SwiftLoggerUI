ee027f09668a683b2f60aabbb886130d32aaea03
Zino <zino@poulet.org>
2020-10-23 08:44:16 +0200

    started working on iOS console, will need retooling of the SwiftLoggerRouter target

---
+++ SwiftLoggeriOS/SwiftLoggeriOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftLoggeriOSApp.swift
+//  SwiftLoggeriOS
+//
+//  Created by Nicolas Zinovieff on 10/23/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftLoggeriOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
